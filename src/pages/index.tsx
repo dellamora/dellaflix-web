cfaf82169e798cf278276379113d793c65280e7d
dellamora <francielle@dellamora.dev>
2022-11-10 21:18:37 -0300

    added axios and verify if api results properly

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import axios from 'axios'
+import { useEffect } from 'react'
+
+export default function Home() {
+    // useEffect(() => {
+    //   const fetch = async () => {
+    //     const result = await axios.get("http://localhost:4000/movie/getPopularMovies")
+    //     console.log(result.data)
+    //   }
+    //   fetch()
+    // }, [])
+    
+  return (
+      <div></div>
+  )
+}
