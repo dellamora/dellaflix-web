59efc1165f7c85da910ea41ca3b80994b9ce9e55
dellamora <francielle@dellamora.dev>
2022-11-20 20:32:04 -0300

    added react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import type { AppProps } from 'next/app'
 import "../common/styles/globals.css";
 import BaseLayout from '../common/layouts/baseLayout'
+import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query'
 
+const queryClient = new QueryClient()
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    <QueryClientProvider client={queryClient}>
     <BaseLayout>
      <Component {...pageProps} />
     </BaseLayout>
+    </QueryClientProvider>
 
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,35 @@
-import axios from 'axios'
-import { title } from 'process'
 import { useEffect } from 'react'
+import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query'
 import MovieCard from '../common/components/movieCard'
 import Hero from '../modules/hero/intex'
-import Nav from '../modules/nav'
+import { BaseSearchResponse, MovieSearch } from '../domain/interfaces'
+
+const queryClient = new QueryClient()
+
 
 export default function Home() {
-    // useEffect(() => {
-    //   const fetch = async () => {
-    //     const result = await axios.get("http://localhost:4000/movie/getPopularMovies")
-    //     console.log(result.data)
-    //   }
-    //   fetch()
-    // }, [])
-    
+  const { isLoading, error, data } = useQuery<BaseSearchResponse<MovieSearch[]>>({
+    queryKey: ['repoData'],
+    queryFn: () =>
+      fetch("http://localhost:4000/movie/getPopularMovies").then(res =>
+        res.json()
+      )
+  })
+
+  if (isLoading) return 'Loading...'
+
+  if (error) return 'An error has occurred: '
+
   return (
     <div>
      <Hero/>
-     <MovieCard movie={{title: "frafdsd",genre_ids:["horror", "asas", "sdsss"],poster_path:"https://mdbootstrap.com/img/new/standard/nature/182.jpg" }}/>
-     
+     { data.data.map( (movie, i) => {
+      return (
+        <MovieCard movie={movie} isFirst={i === 0}/>
+      )
+     })
+      }
+
     </div>
   )
 }
