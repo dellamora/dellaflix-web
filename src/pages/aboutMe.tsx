8bfc1e2a8e9b69ff20eafe920840316b4c62ef78
Dellamora <francielle@dellamora.dev>
2023-01-04 17:17:46 -0300

    finish style details and add description

--- src/pages/aboutMe.tsx
+++ src/pages/aboutMe.tsx
@@ -7,31 +7,35 @@ import CoffeeIcon from "../common/svgs/coffeeIcon";
 const pages: React.FC = (): JSX.Element => {
   return (
     <div className=" mt-[72px] flex justify-center items-center">
-      <div className="grid gap-3 grid-cols-2 grid-rows-3 lg:grid-cols-3 lg:grid-rows-2 h-[80vh] w-full p-20 ">
-        <div className=" bg-gray-600/25  col-span-2 row-span-2 rounded flex justify-between items-center p-10 lg:p-20 ">
-          <div className="text-white font-Orya flex flex-col gap-2 ">
-            <h1 className=" text-5xl font-Orya text-white">Hi, Im Dellamora</h1>
-            <p className=" text-lg font-Inter w-2/3 lg:w-4/5 text-justify">
-              I have a passion for developing weird projects and tech that makes
-              a positive impact on peopless lives.
+      <div className="grid gap-3 grid-cols-2 grid-rows-3 lg:grid-cols-3 lg:grid-rows-2 h-[80vh] w-full p-10  ">
+        <div className=" bg-gray-600/25  col-span-2 row-span-2 gap-10 rounded flex justify-between items-center p-10 ">
+          <div className="text-white font-Inter text-thin flex flex-col">
+            <h1 className="text-xl lg:text-4xl xl:text-5xl font-Inter text-white">
+              Hi, I&apos;m Dellamora
+            </h1>
+            <p className="text-sm text-thin lg:text-base font-Inter w-full  text-justify">
+              My passion for computers began when I was a child and used my
+              father&apos;s computer to play games. Over the years I learned how
+              to create themes on the Tumblr platform and I consider it a
+              milestone in my choice of profession, since I was messing around
+              with HTML and CSS and got enchanted with the web universe.
+              Currently, I have over 2 years of experience working in the
+              software development industry, seeking efficiency in developing
+              ideas and turning them into reality, bringing smart solutions to
+              challenges and adversities.
             </p>
-            <p className="text-lg font-Inter w-2/3 lg:w-4/5 text-justify">
-              I’m interested in freelance opportunities – especially ambitious
-              or large projects. However, if you have other request or question,
-              don’t hesitate to use the form.
-            </p>
-            <a
-              className="cursor-pointer font- text-3xl text-ellipsis text-white"
-              href="mailto:francielle@dellamora.dev"
-            >
-              Contact me
-            </a>
           </div>
           <img
-            className="w-[200px] lg:w-[300px] rounded-full object-cover mr-4 shadow aspect-square"
+            className="w-[150px] lg:w-[200px] xl:w-[300px] rounded-full object-cover shadow aspect-square"
             src="/assets/mePc.jpg"
             alt="avatar"
           />
+        </div>
+        <div className="group bg-gray-800/25 rounded flex flex-col items-center justify-center">
+          <ClickIcon className="group-hover:stroke-slate-500" />
+          <h1 className="text-white font-Inter group-hover:text-slate-500 ">
+            My personal website
+          </h1>
         </div>
         <a
           href="https://www.buymeacoffee.com/dellamora"
@@ -42,12 +46,6 @@ const pages: React.FC = (): JSX.Element => {
             Buy me a coffee!
           </h1>
         </a>
-        <div className="group bg-gray-800/25 rounded flex flex-col items-center justify-center">
-          <ClickIcon className="group-hover:stroke-slate-500" />
-          <h1 className="text-white font-Inter group-hover:text-slate-500 ">
-            More About Me
-          </h1>
-        </div>
       </div>
     </div>
   );
