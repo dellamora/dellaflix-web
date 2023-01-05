c7caf13b832ef06219f53b1e2928a4205f2fa4ea
Dellamora <francielle@dellamora.dev>
2023-01-05 18:50:55 -0300

    finish top10Movies component frontend

--- src/common/components/top10Movies/index.tsx
+++ src/common/components/top10Movies/index.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
-import React from "react";
+import React, { useRef, useState } from "react";
 import One from "../../svgs/one";
 import Three from "../../svgs/three";
 import Seven from "../../svgs/seven";
@@ -11,21 +11,49 @@ import Six from "../../svgs/six";
 import Eight from "../../svgs/eight";
 import Nine from "../../svgs/nine";
 import Ten from "../../svgs/ten";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 const numbers = [One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten];
 
 const Top10Movies: React.FC = (): JSX.Element => {
+  const [swiperRef, setSwiperRef] = useState(null);
+
   return (
-    <div className="flex">
-      {numbers.map((Number, i) => {
-        return (
-          <div key={`number-${i}`} className="flex">
-            <Number />
-            <div className="h-[180px] w-[130px] bg-teal-500">poster</div>
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <h1 className=" text-white text-2xl font-semibold mb-3 ml-20 ">
+        Top 10 Movies in Brazil Today
+      </h1>
+      <div className="flex ">
+        <Swiper
+          onSwiper={setSwiperRef}
+          slidesPerView={5}
+          centeredSlides={false}
+          spaceBetween={30}
+          navigation={true}
+          modules={[Navigation]}
+          className="SwiperTopTen"
+        >
+          {numbers.map((Number, i) => {
+            return (
+              <SwiperSlide
+                key={`number-${i}`}
+                className="flex "
+                style={{
+                  marginLeft: i === 0 ? "90px" : "0px",
+                }}
+              >
+                <Number />
+                <div className="h-[180px] w-[130px] bg-teal-500">poster</div>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </div>
+    </>
   );
 };
 
