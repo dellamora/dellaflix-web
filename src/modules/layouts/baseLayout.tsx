504773add846a4c9e500979dce2a43d869c2ea2b
Dellamora <francielle@dellamora.dev>
2023-01-11 14:16:29 -0300

    added again basedLayout

---
+++ src/modules/layouts/baseLayout.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import React from "react";
+import Footer from "../../modules/footer";
+import Nav from "../../modules/nav";
+import { useInView } from "react-intersection-observer";
+
+const BaseLayout = ({
+  children,
+}: {
+  children: React.ReactNode;
+}): JSX.Element => {
+  const { ref, inView } = useInView({});
+  return (
+    <div className="h-screen flex flex-col  bg-[#141414]">
+      <Nav showBg={!inView} />
+      <div className="grow w-screen overflow-x-hidden">
+        <div ref={ref} className=" h-[1px]" />
+        {children}
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default BaseLayout;
