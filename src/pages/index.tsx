b2727e27152a8df82c1348a8dfc6b98291b8d654
Dellamora <francielle@dellamora.dev>
2023-01-11 14:16:41 -0300

    deleted useless lines

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,7 @@
 import { useEffect, useMemo, useState } from "react";
 import { QueryClient, useQuery } from "@tanstack/react-query";
-import MovieCard from "../common/components/movieCard";
 import Hero from "../modules/hero/intex";
 import { BaseSearchResponse, MovieSearch } from "../domain/interfaces";
-import Carousel from "../common/components/carousel";
-import { SwiperSlide } from "swiper/react";
 import Top10Movies from "../common/components/top10Movies";
 import { useMovieByGenre } from "../common/hooks/useMovieByGenre";
 import MovieCarousel from "../common/components/movieCarousel";
@@ -28,7 +25,6 @@ export default function Home() {
   const isLoading = useMemo(() => {
     return baseLoading || animationMovies.isLoading;
   }, [baseLoading, animationMovies.isLoading]);
-  console.log(animationMovies);
   if (isLoading) return "Loading...";
 
   if (error) return "An error has occurred: ";
