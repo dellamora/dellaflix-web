b465dea7aaeb1a1f3c1f0948b6a8fe254c42e491
Dellamora <francielle@dellamora.dev>
2023-01-11 14:15:08 -0300

    added MovieCarousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,34 @@
-import { useEffect, useState } from "react";
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from "@tanstack/react-query";
+import { useEffect, useMemo, useState } from "react";
+import { QueryClient, useQuery } from "@tanstack/react-query";
 import MovieCard from "../common/components/movieCard";
 import Hero from "../modules/hero/intex";
 import { BaseSearchResponse, MovieSearch } from "../domain/interfaces";
 import Carousel from "../common/components/carousel";
 import { SwiperSlide } from "swiper/react";
 import Top10Movies from "../common/components/top10Movies";
-import CardHover from "../common/components/movieCard/cardHover";
-
+import { useMovieByGenre } from "../common/hooks/useMovieByGenre";
+import MovieCarousel from "../common/components/movieCarousel";
 const queryClient = new QueryClient();
 
 export default function Home() {
   const [activeIndex, setActiveIndex] = useState(0);
 
-  const { isLoading, error, data } = useQuery<
-    BaseSearchResponse<MovieSearch[]>
-  >({
+  const {
+    isLoading: baseLoading,
+    error,
+    data,
+  } = useQuery<{ data: BaseSearchResponse<MovieSearch[]> }>({
     queryKey: ["repoData"],
     queryFn: () =>
       fetch("http://localhost:4000/movie/getPopularMovies").then(res =>
         res.json(),
       ),
   });
-
+  const animationMovies = useMovieByGenre("Animation");
+  const isLoading = useMemo(() => {
+    return baseLoading || animationMovies.isLoading;
+  }, [baseLoading, animationMovies.isLoading]);
+  console.log(animationMovies);
   if (isLoading) return "Loading...";
 
   if (error) return "An error has occurred: ";
@@ -34,79 +36,14 @@ export default function Home() {
     <>
       <Hero />
       <div className="flex flex-col-reverse">
-        <Carousel
-          category="Outro Exemplo"
-          onActiveChange={index => {
-            setActiveIndex(index);
-            console.log(index);
-          }}
-        >
-          {data.data.map((movie, i) => {
-            return (
-              <SwiperSlide key={`MovieCard-${i}`}>
-                <MovieCard
-                  movie={movie}
-                  position={
-                    activeIndex === i + 1
-                      ? "right"
-                      : activeIndex === i + 5
-                      ? "left"
-                      : "middle"
-                  }
-                />
-              </SwiperSlide>
-            );
-          })}
-        </Carousel>
-        <Top10Movies movies={data.data} />
-        <Carousel
-          category="Exemplo"
-          onActiveChange={index => {
-            setActiveIndex(index);
-            console.log(index);
-          }}
-        >
-          {data.data.map((movie, i) => {
-            return (
-              <SwiperSlide key={`MovieCard-${i}`}>
-                <MovieCard
-                  movie={movie}
-                  position={
-                    activeIndex === i + 1
-                      ? "right"
-                      : activeIndex === i + 5
-                      ? "left"
-                      : "middle"
-                  }
-                />
-              </SwiperSlide>
-            );
-          })}
-        </Carousel>
-        <Carousel
-          category="Popular On Dellaflix"
-          onActiveChange={index => {
-            setActiveIndex(index);
-            console.log(index);
-          }}
-        >
-          {data.data.map((movie, i) => {
-            return (
-              <SwiperSlide key={`MovieCard-${i}`}>
-                <MovieCard
-                  movie={movie}
-                  position={
-                    activeIndex === i + 1
-                      ? "right"
-                      : activeIndex === i + 5
-                      ? "left"
-                      : "middle"
-                  }
-                />
-              </SwiperSlide>
-            );
-          })}
-        </Carousel>
+        <MovieCarousel category="Comedy" />
+        <MovieCarousel category="Mystery" />
+        <MovieCarousel category="Animation" />
+        <Top10Movies
+          category="Top 10 Movies Popular On Dellaflix"
+          movies={data.data.results}
+        />
+        <MovieCarousel category="Fantasy" />
       </div>
     </>
   );
