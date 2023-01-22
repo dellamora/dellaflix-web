8a31d56ac72c6de912d752123f52f018e057286a
Dellamora <francielle@dellamora.dev>
2023-01-22 18:20:19 -0300

    deleted usequery import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,7 @@
 import type { AppProps } from "next/app";
 import "../common/styles/globals.css";
 import BaseLayout from "../common/layouts/baseLayout";
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 export default function App({ Component, pageProps }: AppProps) {
